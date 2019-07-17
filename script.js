382134591bf30fe43ca4523f7ff404ef4cea281a
Konda Shivaradhan <38292566+kondashiva@users.noreply.github.com>
2019-07-17 19:07:33 +0530

    navi
    fixed temp

--- script.js
+++ script.js
@@ -4,23 +4,28 @@ function myFunction() {
   }
 }
 function small() {
-  
-  $("#big2").hide(1000);
-  $("#big1").hide(1000);
+  $("#big2").hide(700);
+  $("#big1").hide(700);
+   $("#navi").animate(
+     {
+       "backgroundColor": "linear-gradient(145deg,white,black,white)"
+     },
+     1000
+   );
   document.getElementById("rot").style.transition = "all 1s";
   document.getElementById("navi").style.height = "70px";
-  document.getElementById("navi").style.background =
-    "linear-gradient(90deg,white,black,white)";
+document.getElementById("navi").style.backgroundColor = "rgb(140,140,140)";
+document.getElementById("navi").style.transition = "all 1s";
   document.getElementById("rot").style.transform = "rotate(" + 90 + "deg)";
 }
 function big() {
-
-  $("#big2").show(1000);
-    $("#big1").show(1000);
+  $("#big2").show(700);
+  $("#big1").show(700);
   document.getElementById("rot").style.transition = "all 1s";
   document.getElementById("navi").style.height = "100%";
-  document.getElementById("navi").style.background =
-    "linear-gradient(45deg,white,black,white)";
+  document.getElementById("navi").style.backgroundColor="rgb(70,70,70)";
+  document.getElementById("navi").style.transition="all 1s";
+
   document.getElementById("rot").style.transform = "rotate(" + 270 + "deg)";
 }
 function navi() {
