2fb73347151a5a46143ce2ff616277774fa9943c
Konda Shivaradhan <38292566+kondashiva@users.noreply.github.com>
2019-07-17 16:12:30 +0530

    navi
    added temp buttons with transition
    
    Co-Authored-By: Naveen Nunnaguppala <naveennunnaguppala@gmail.com>

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 function myFunction() {
   if (window.pageYOffset > 20) {
+    document.getElementById("big").style.display = "none";
+    document.getElementById("big").style.transition = "all 1s";
+    document.getElementById("big2").style.display = "none";
+    document.getElementById("big2").style.transition = "all 1s";
     //document.getElementById("keka").style.display = "none";
     //document.getElementById("keka").style.transition = "all 1.5s";
     document.getElementById("navi").style.height = "70px";
-    document.getElementById("navi").style.transition = "all 0.5s";
+    document.getElementById("navi").style.transition = "all 1s";
   } else {
+    document.getElementById("big").style.display = "";
+    document.getElementById("big").style.transition = "all 1s";
+    document.getElementById("big2").style.display = "";
+    document.getElementById("big2").style.transition = "all 1s";
     //document.getElementById("keka").style.display = "";
     //document.getElementById("keka").style.transition = "all 1.5s";
     document.getElementById("navi").style.height = "100%";
-    document.getElementById("navi").style.top = "0px";
-    document.getElementById("navi").style.transition = "all 0.5s";
+   
+    document.getElementById("navi").style.transition = "all 1s";
   }
 }
