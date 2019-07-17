dff8c2e01ab3ee65176b35e23edef4c83a4b37bb
Konda Shivaradhan <38292566+kondashiva@users.noreply.github.com>
2019-07-17 19:17:12 +0530

    font changed

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ function myFunction() {
 function small() {
   $("#big2").hide(700);
   $("#big1").hide(700);
-   $("#navi").animate(
-     {
-       "backgroundColor": "linear-gradient(145deg,white,black,white)"
-     },
-     1000
-   );
+  $("#navi").animate(
+    {
+      backgroundColor: "linear-gradient(145deg,white,black,white)"
+    },
+    1000
+  );
   document.getElementById("rot").style.transition = "all 1s";
   document.getElementById("navi").style.height = "70px";
-document.getElementById("navi").style.backgroundColor = "rgb(140,140,140)";
-document.getElementById("navi").style.transition = "all 1s";
+  //document.getElementById("navi").style.backgroundColor = "rgb(140,140,140)";
+  document.getElementById("navi").style.transition = "all 1s";
   document.getElementById("rot").style.transform = "rotate(" + 90 + "deg)";
 }
 function big() {
@@ -23,8 +23,8 @@ function big() {
   $("#big1").show(700);
   document.getElementById("rot").style.transition = "all 1s";
   document.getElementById("navi").style.height = "100%";
-  document.getElementById("navi").style.backgroundColor="rgb(70,70,70)";
-  document.getElementById("navi").style.transition="all 1s";
+ // document.getElementById("navi").style.backgroundColor = "rgb(70,70,70)";
+  document.getElementById("navi").style.transition = "all 1s";
 
   document.getElementById("rot").style.transform = "rotate(" + 270 + "deg)";
 }
