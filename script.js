207400db26e5a6a9d085c56ff473a3a5123a5742
Konda Shivaradhan <38292566+kondashiva@users.noreply.github.com>
2019-07-17 17:20:08 +0530

    navi updated
    added small to bid nav bar button
    
    Co-Authored-By: Naveen Nunnaguppala <naveennunnaguppala@gmail.com>

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 function myFunction() {
   if (window.pageYOffset > 20) {
-    document.getElementById("big2").style.display = "none";
-    document.getElementById("big2").style.transition = "all 1s";
-    document.getElementById("big").style.display = "none";
-    document.getElementById("big").style.transition = "all 1s";
-    document.getElementById("navi").style.height = "70px";
-    document.getElementById("navi").style.transition = "all 1s";
+    small();
   } else {
-    document.getElementById("big").style.display = "";
-    document.getElementById("big").style.transition = "all 1s";
-    document.getElementById("big2").style.display = "";
-    document.getElementById("big2").style.transition = "all 1s";
-    document.getElementById("navi").style.height = "100%";
-    document.getElementById("navi").style.transition = "all 1s";
+  big();
+  }
+}
+function small()
+{
+  document.getElementById("big2").style.display = "none";
+  document.getElementById("big2").style.transition = "all 1s";
+  document.getElementById("big").style.display = "none";
+  document.getElementById("big").style.transition = "all 1s";
+  document.getElementById("navi").style.height = "70px";
+  document.getElementById("navi").style.transition = "all 1s";
+  
+   document.getElementById("rot").style.transform = "rotate(" +   90 + "deg)";
+}
+function big()
+{
+   document.getElementById("big").style.display = "";
+   document.getElementById("big").style.transition = "all 1s";
+   document.getElementById("big2").style.display = "";
+   document.getElementById("big2").style.transition = "all 1s";
+   document.getElementById("navi").style.height = "250px";
+   document.getElementById("navi").style.transition = "all 1s";
+   document.getElementById("rot").style.transform='rotate('+270+'deg)';
+   document.getElementById("rot").style.transition = "all 1s";
+}
+function navi()
+{
+  if(document.getElementById("navi").offsetHeight=="70")
+  {
+    big();
+  }
+  else{
+    small();
   }
 }
