818e952bddf923afcf7d500c517c5ef7bd208325
Konda Shivaradhan <38292566+KondaShivaradhan@users.noreply.github.com>
2020-06-15 21:00:46 +0530

    chages
    code reboot

--- script.js
+++ script.js
@@ -3,41 +3,26 @@ function myFunction() {
         small();
     }
 }
-
 function small() {
-    $("#big2").hide(700);
-    $("#img").hide(700);
-    $("#simg").show(300);
-    $("#big1").hide(700);
+    $('#ntext').hide().css({
+        opacity: '1'
+    });
     $("#navi").animate({
-            backgroundColor: "linear-gradient(145deg,white,black,white)"
-        },
+        backgroundColor: "linear-gradient(145deg,white,black,white)"
+    },
         1000
     );
-    /*document.getElementById("simg").style.visibility = "visible";
-    document.getElementById("simg").style.transition = "all 1s";*/
-    document.getElementById("rot").style.transition = "all 1s";
+    document.getElementById("svg").style.cssText = 'height:0%; width:0%;transition:all 0.3s';
+    document.getElementById("rot").style.transition = "all 0.5s";
     document.getElementById("navi").style.height = "70px";
-    //document.getElementById("navi").style.backgroundColor = "rgb(140,140,140)";
-    document.getElementById("navi").style.transition = "all 1s";
     document.getElementById("rot").style.transform = "rotate(" + 90 + "deg)";
 }
-
 function big() {
-    $("#simg").hide(300);
-    $("#img").show(700);
-    $("#big2").show(700);
-    $("#big1").show(700);
-    document.getElementById("rot").style.transition = "all 1s";
+    document.getElementById("svg").style.cssText = 'height:50%; width:50%;';
+    $('#ntext').show();
     document.getElementById("navi").style.height = "100%";
-    /*document.getElementById("simg").style.visibility = "hidden";
-    document.getElementById("simg").style.transition = "all 1s";*/
-    // document.getElementById("navi").style.backgroundColor = "rgb(70,70,70)";
-    document.getElementById("navi").style.transition = "all 1s";
-
-    document.getElementById("rot").style.transform = "rotate(" + 270 + "deg)";
+    document.getElementById("rot").style.transform = "rotate(" + 270 + "deg)"; 
 }
-
 function navi() {
     if (document.getElementById("navi").offsetHeight == "70") {
         big();
