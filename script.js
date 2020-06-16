dd2ea2a9f4ccc11decd0cbbf27034349483187af
Konda Shivaradhan <38292566+KondaShivaradhan@users.noreply.github.com>
2020-06-16 16:37:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -12,12 +12,16 @@ function small() {
     },
         1000
     );
+    document.getElementById('scrolldiv').style.display = 'none';
+    document.getElementById("small").style.display = 'flex';
     document.getElementById("svg").style.cssText = 'height:0%; width:0%;transition:all 0.3s';
     document.getElementById("rot").style.transition = "all 0.5s";
     document.getElementById("navi").style.height = "70px";
     document.getElementById("rot").style.transform = "rotate(" + 90 + "deg)";
 }
 function big() {
+    document.getElementById('scrolldiv').style.display = '';
+    document.getElementById("small").style.display = 'none';
     document.getElementById("svg").style.cssText = 'height:50%; width:50%;';
     $('#ntext').show();
     document.getElementById("navi").style.height = "100%";
