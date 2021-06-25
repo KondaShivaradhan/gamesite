9f164ad4d8657155dd4827598915521901635421
Konda Shivaradhan <38292566+KondaShivaradhan@users.noreply.github.com>
2021-06-26 00:18:27 +0530

    changing to Blazingbane
    Gamesite is changed to BB,
    logo changed,
    smoothed

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
 function myFunction() {
-    if (window.pageYOffset > 20) {
-        small();
-    }
+    // if (window.pageYOffset > 20) {
+    //     small();
+    // }
+    small()
 }
+
 function small() {
+
     $('#ntext').hide().css({
         opacity: '1'
     });
     $("#navi").animate({
-        backgroundColor: "linear-gradient(145deg,white,black,white)"
-    },
+            backgroundColor: "linear-gradient(145deg,white,black,white)"
+        },
         1000
     );
-    document.getElementById('scrolldiv').style.display = 'none';
+    // document.getElementById('scrolldiv').style.display = 'none';
     document.getElementById("small").style.display = 'flex';
     document.getElementById("svg").style.cssText = 'height:0%; width:0%;transition:all 0.3s';
     document.getElementById("rot").style.transition = "all 0.5s";
     document.getElementById("navi").style.height = "70px";
     document.getElementById("rot").style.transform = "rotate(" + 90 + "deg)";
 }
+
 function big() {
-    document.getElementById('scrolldiv').style.display = '';
+    console.log("big called");
+    // document.getElementById('scrolldiv').style.display = '';
     document.getElementById("small").style.display = 'none';
     document.getElementById("svg").style.cssText = 'height:50%; width:50%;';
     $('#ntext').show();
     document.getElementById("navi").style.height = "100%";
-    document.getElementById("rot").style.transform = "rotate(" + 270 + "deg)"; 
+    document.getElementById("rot").style.transform = "rotate(" + 270 + "deg)";
 }
+
 function navi() {
     if (document.getElementById("navi").offsetHeight == "70") {
         big();
